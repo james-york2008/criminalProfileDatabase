11b12e2c9b762a1637acf04d05fd2cae823b9ae7
James <jwyork2008@gmail.com>
2026-03-08 14:49:35 -0500

    Remade index.html, and designed navbar for mobile

---
+++ src/index/hamburgerMenu.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.getElementById('dropdownButton')
+    let navList = document.getElementById('navList')
+
+    btn.addEventListener('click', function() {
+        if (navList.classList.contains('d-none')) {
+            navList.classList.remove('d-none')
+            navList.classList.add('d-show')
+        } else {
+            navList.classList.remove('d-show')
+            navList.classList.add('d-none')    
+        }
+    })
+})
