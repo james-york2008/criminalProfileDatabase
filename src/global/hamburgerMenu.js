5c17314274bbd78178d2e275b54de315bf5eb86f
James <jwyork2008@gmail.com>
2026-03-17 12:50:15 -0500

    Attempted to fix dropdown button issues on small screens and footer wrapping

--- src/global/hamburgerMenu.js
+++ src/global/hamburgerMenu.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     let btn = document.getElementById('dropdownButton')
     let navList = document.getElementById('navList')
 
-    btn.addEventListener('click', function() {
-        if (navList.classList.contains('d-none')) {
-            navList.classList.remove('d-none')
-            navList.classList.add('d-show')
-        } else {
-            navList.classList.remove('d-show')
-            navList.classList.add('d-none')    
-        }
+    btn.addEventListener('click', function() {   
+            if (navList.classList.contains('d-none')) {
+                navList.classList.remove('d-none')
+                navList.classList.add('d-show')
+            } else {
+                navList.classList.remove('d-show')
+                navList.classList.add('d-none')    
+            }
     })
 })
